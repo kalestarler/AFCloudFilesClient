122ad71dd0a0e191cb8a8fdee0bc147198bb6417
Kale Toh <kale@rs-v.com>
2012-07-12 21:09:57 +0800

    Added method to retrieve image from container.
    Merged authenticate method into init.

--- AFCloudFilesClient.h
+++ AFCloudFilesClient.h
@@ -11,7 +11,8 @@
 @protocol AFCloudFilesClientDelegate;
 
 @interface AFCloudFilesClient : NSObject {
-
+    
+    BOOL authenticated;
 }
 
 @property(nonatomic, retain) NSString *cfUsername;
@@ -24,18 +25,22 @@
 @property(nonatomic, retain) UIProgressView *progressBar;
 
 -(id)initWithUsername:(NSString *)username andKey:(NSString *)apiKey;
-
 -(void)authenticate;
+
 -(void)uploadFileToContainer:(NSString *)container withFilename:(NSString *)filename data:(NSData *)data andContentType:(NSString *)contentType;
 -(void)setProgressBar:(UIProgressView *)appProgressBar;
+
+-(void)retrieveImageWithFilename:(NSString *)filename fromContainer:(NSString *)container;
 
 @end
 
 @protocol AFCloudFilesClientDelegate <NSObject>
--(void)AFCloudFilesClientAuthenticationSucceeded:(AFCloudFilesClient *)client;
--(void)AFCloudFilesClientAuthenticationFailed:(AFCloudFilesClient *)client;
 @optional
+
 -(void)AFCloudFilesClient:(AFCloudFilesClient *)client withAFHTTPRequestOperation:(AFHTTPRequestOperation *)operation completedUploadWithResponse:(id)responseObject;
 -(void)AFCloudFilesClient:(AFCloudFilesClient *)client withAFHTTPRequestOperation:(AFHTTPRequestOperation *)operation failedUploadWithError:(NSError *)error;
+
+-(void)retrievedImage:(UIImage *)image WithFilename:(NSString *)filename fromContainer:(NSString *)container;
+-(void)AFCloudFilesClient:(AFCloudFilesClient *)client withAFHTTPRequestOperation:(AFHTTPRequestOperation *)operation failedDownloadWithError:(NSError *)error;
 
 @end
--- AFCloudFilesClient.m
+++ AFCloudFilesClient.m
@@ -31,6 +31,7 @@
     [super init];
     if(self) {
         
+        authenticated = NO;
         cfUsername = [[NSString alloc] initWithString:username];
         cfAPIKey = [[NSString alloc] initWithString:apiKey];
         authToken = [[NSString alloc] init];
@@ -38,15 +39,16 @@
         cdnManagementURL = [[NSString alloc] init];
         cfClient = [[AFHTTPClient alloc] init];
         progressBar = [[UIProgressView alloc] init];
-        
+        [self authenticate];
     }
     return self;
 }
 
+#pragma mark - authentication
+
 -(void)authenticate {
     
-    BOOL authenticated = NO;
-    
+    //check for saved auth token first
     if([[NSUserDefaults standardUserDefaults] objectForKey:@"AFRackspaceAuthTokenExpiryKey"]) {
         
         NSDate *currentDate = [NSDate date];
@@ -54,21 +56,31 @@
         NSDate *expiryDatePlusOneDay = [expiryDate dateByAddingTimeInterval:60*60*24];
         
         if([expiryDatePlusOneDay compare:currentDate] == NSOrderedAscending) {
+            
             //expiry is earlier
             authenticated = NO;
         }
         else if([expiryDatePlusOneDay compare:currentDate] == NSOrderedDescending) {
+            
             //expiry is later
+            
+            authToken = [[NSUserDefaults standardUserDefaults] objectForKey:@"AFRackspaceAuthTokenKey"];
+            storageURL = [[NSUserDefaults standardUserDefaults] objectForKey:@"AFRackspaceStorageURLKey"];
+            cdnManagementURL = [[NSUserDefaults standardUserDefaults] objectForKey:@"AFRackspaceCDNManagementURLKey"];
+            
             authenticated = YES;
         }
     }
     
+    //if there is no valid saved auth token, acquire new one
     if(!authenticated) {
+        
         NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:CLOUDFILES_AUTH_URL]];
         [request setValue:cfUsername forHTTPHeaderField:@"X-Auth-User"];
         [request setValue:cfAPIKey forHTTPHeaderField:@"X-Auth-Key"];
         
         AFHTTPRequestOperation *authOp = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+        
         [authOp setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
             
             authToken = [[operation.response allHeaderFields] objectForKey:@"X-Auth-Token"];
@@ -80,75 +92,131 @@
             [[NSUserDefaults standardUserDefaults] setObject:storageURL forKey:@"AFRackspaceStorageURLKey"];
             [[NSUserDefaults standardUserDefaults] setObject:cdnManagementURL forKey:@"AFRackspaceCDNManagementURLKey"];
             
-            if([delegate respondsToSelector:@selector(AFCloudFilesClientAuthenticationSucceeded:)]) {
-                
-                [delegate AFCloudFilesClientAuthenticationSucceeded:self];
-            }
+            authenticated = YES;
         } 
                                       failure:^(AFHTTPRequestOperation *operation, NSError *error) {
                                           
-                                          if([delegate respondsToSelector:@selector(AFCloudFilesClientAuthenticationFailed:)]) {
-                                              
-                                              [delegate AFCloudFilesClientAuthenticationFailed:self];
-                                          }
+                                          //if fail, try to authenticate again
+                                          [self performSelector:@selector(authenticate) withObject:nil afterDelay:1.0];
                                       }];
         
         [authOp start];
         [request release];
         [authOp release];
     }
-    else {
-        
-        authToken = [[NSUserDefaults standardUserDefaults] objectForKey:@"AFRackspaceAuthTokenKey"];
-        storageURL = [[NSUserDefaults standardUserDefaults] objectForKey:@"AFRackspaceStorageURLKey"];
-        cdnManagementURL = [[NSUserDefaults standardUserDefaults] objectForKey:@"AFRackspaceCDNManagementURLKey"];
-        
-        if([delegate respondsToSelector:@selector(AFCloudFilesClientAuthenticationSucceeded:)]) {
-            
-            [delegate AFCloudFilesClientAuthenticationSucceeded:self];
-        }
-    }
 }
+
+#pragma mark - upload file to container
 
 -(void)uploadFileToContainer:(NSString *)container withFilename:(NSString *)filename data:(NSData *)data andContentType:(NSString *)contentType {
     
-    NSString *urlString = [NSString stringWithFormat:@"%@/%@/%@", storageURL, container, filename];
-    NSURL *uploadURLAndPath = [NSURL URLWithString:urlString];
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:uploadURLAndPath];
-    [request setValue:authToken forHTTPHeaderField:@"X-Auth-Token"];
-    [request setValue:contentType forHTTPHeaderField:@"Content-Type"];
-    [request setHTTPMethod:@"PUT"];
-    [request setHTTPBody:data];
-    
-    AFHTTPRequestOperation *uploadOp = [[AFHTTPRequestOperation alloc] initWithRequest:request];
-    [uploadOp setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+    if (!authenticated) {
         
-        if([delegate respondsToSelector:@selector(AFCloudFilesClient:withAFHTTPRequestOperation:completedUploadWithResponse:)]) {
+        if([delegate respondsToSelector:@selector(AFCloudFilesClient:withAFHTTPRequestOperation:failedUploadWithError:)]) {
             
-            [delegate AFCloudFilesClient:self withAFHTTPRequestOperation:operation completedUploadWithResponse:responseObject];
+            NSError *error = [[NSError alloc] init];
+            [error setValue:@"AFCloudFilesClient not authenticated. Upload operation aborted." forKey:NSLocalizedDescriptionKey];
+            
+            [delegate AFCloudFilesClient:self withAFHTTPRequestOperation:nil failedUploadWithError:error];
+            
+            [error release];
         }
-    
-    } 
-                                    failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-                                        
-                                        if([delegate respondsToSelector:@selector(AFCloudFilesClient:withAFHTTPRequestOperation:failedUploadWithError:)]) {
-                                            
-                                            [delegate AFCloudFilesClient:self withAFHTTPRequestOperation:operation failedUploadWithError:error];
-                                        }
-                                    }];
-    [uploadOp setUploadProgressBlock:^(NSInteger bytesWritten, long long totalBytesWritten, long long totalBytesExpectedToWrite) {
+    }
+    else {
+            
+        NSString *urlString = [NSString stringWithFormat:@"%@/%@/%@", storageURL, container, filename];
+        NSURL *uploadURLAndPath = [NSURL URLWithString:urlString];
+        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:uploadURLAndPath];
+        [request setValue:authToken forHTTPHeaderField:@"X-Auth-Token"];
+        [request setValue:contentType forHTTPHeaderField:@"Content-Type"];
+        [request setHTTPMethod:@"PUT"];
+        [request setHTTPBody:data];
         
-        progressBar.progress = totalBytesWritten/totalBytesExpectedToWrite;
-    }];
-    
-    [uploadOp start];
-    [request release];
-    [uploadOp release];
+        AFHTTPRequestOperation *uploadOp = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+        
+        [uploadOp setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+            
+            if([delegate respondsToSelector:@selector(AFCloudFilesClient:withAFHTTPRequestOperation:completedUploadWithResponse:)]) {
+                
+                [delegate AFCloudFilesClient:self withAFHTTPRequestOperation:operation completedUploadWithResponse:responseObject];
+            }
+            
+        } 
+                                        failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                                            
+                                            if([delegate respondsToSelector:@selector(AFCloudFilesClient:withAFHTTPRequestOperation:failedUploadWithError:)]) {
+                                                
+                                                [delegate AFCloudFilesClient:self withAFHTTPRequestOperation:operation failedUploadWithError:error];
+                                            }
+                                        }];
+        
+        [uploadOp setUploadProgressBlock:^(NSInteger bytesWritten, long long totalBytesWritten, long long totalBytesExpectedToWrite) {
+            
+            progressBar.progress = totalBytesWritten/totalBytesExpectedToWrite;
+        }];
+        
+        [uploadOp start];
+        [request release];
+        [uploadOp release];
+    }
 }
 
 -(void)setProgressBar:(UIProgressView *)appProgressBar {
     
     progressBar = appProgressBar;
+}
+
+#pragma mark - retrieve object from container
+
+-(void)retrieveImageWithFilename:(NSString *)filename fromContainer:(NSString *)container {
+    
+     __block UIImage *image;
+    
+    if (!authenticated) {
+        
+        if([delegate respondsToSelector:@selector(AFCloudFilesClient:withAFHTTPRequestOperation:failedDownloadWithError:)]) {
+            
+            NSError *error = [[NSError alloc] init];
+            [error setValue:@"AFCloudFilesClient not authenticated. Download operation aborted." forKey:NSLocalizedDescriptionKey];
+            
+            [delegate AFCloudFilesClient:self withAFHTTPRequestOperation:nil failedDownloadWithError:error];
+            
+            [error release];
+        }
+    }
+    else {
+        
+        NSString *urlString = [NSString stringWithFormat:@"%@/%@/%@", storageURL, container, filename];
+        NSURL *downloadURLAndPath = [NSURL URLWithString:urlString];
+        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:downloadURLAndPath];
+        [request setValue:authToken forHTTPHeaderField:@"X-Auth-Token"];
+        [request setHTTPMethod:@"GET"];
+        
+        AFHTTPRequestOperation *downloadOp = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+        
+        [downloadOp setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+            
+            UIImage *responseImage = [[UIImage alloc] initWithData:responseObject];
+            image = responseImage;
+            [responseImage release];
+            
+            if ([delegate respondsToSelector:@selector(retrievedImage:WithFilename:fromContainer:)]) {
+                
+                [delegate retrievedImage:image WithFilename:filename fromContainer:container];
+            }
+            
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            
+            if([delegate respondsToSelector:@selector(AFCloudFilesClient:withAFHTTPRequestOperation:failedDownloadWithError:)]) {
+            
+                [delegate AFCloudFilesClient:self withAFHTTPRequestOperation:operation failedDownloadWithError:error];
+            }
+        }];
+        
+        [downloadOp start];
+        [request release];
+        [downloadOp release];
+    }
 }
 
 @end
